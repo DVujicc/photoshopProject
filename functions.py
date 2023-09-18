ecda206b80bb4d84aa831b4b7cf45701530fdd5c
Ana-Marija <ramdagadamdagadam@gmail.com>
2023-09-18 21:46:37 +0200

    novi features

--- functions.py
+++ functions.py
@@ -9,7 +9,7 @@ from PIL import Image
 
 ###### crop ######
 
-def crop_jpg(image_location, height, width):
+def crop(image_location, height, width):
     image = Image.open(image_location)
     box = (height, height, width, width)
     cropped_image = image.crop(box)
@@ -32,13 +32,34 @@ def flip_vertically(image_location):
 
 ##### rotate #####
 
-def rotate_jpg(image_location, angle):
+def rotate(image_location, angle):
     expand = True
     im = Image.open(image_location)
     out = im.rotate(angle, expand=True)
     out.save('rotate-output.png')
     return out 
 
+##### brightness #####
+
+def blend(image_location, brightness, value):  #### podeli input sa 100          
+    it1 = np.nditer(image_location)
+    it2 = np.nditer(brightness)            
+    for (x) in it1:
+        for (y) in it2:
+            newImage = (x + y) * value
+    return out
+
+##### contrast #####
+
+def contrast(image_location, value): #### podeli input sa 10
+    im = Image.open(image_location)
+    image_array = np.array(im)
+    contrast_factor = value
+    contrast_img = image_array * contrast_factor
+    contrast_img = np.clip (contrast_img, 0, 255)
+    out = Image.fromarray(contrast_img.astype(np.uint8))
+    out.save("output_image.jpg")
+    return out
 
 
 
