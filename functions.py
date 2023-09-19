81d01179a3a250beb3291b7ba42031f6ee0a0842
DVujicc <dimitrijev005@gmail.com>
2023-09-19 15:19:51 +0200

    v1.0

--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
 import PIL 
 import numpy as np
 import cv2
-from PIL import Image 
+from PIL import Image, ImageEnhance
 
 ######## kako napraviti da funkcionise za png i jpg ?????????????? #########
 
@@ -21,44 +21,47 @@ def crop(image_location, height, width):
 def flip_horizontally(image_location):
     im = Image.open(image_location)
     out = im.transpose(PIL.Image.FLIP_TOP_BOTTOM)
-    out.save('transpose-output.jpg')
+    out.save(image_location)
     return out 
     
-def flip_vertically(image_location):
-    im = Image.open(image_location)
+def flip_vertically(file_path):
+    im = Image.open(file_path)
     out = im.transpose(PIL.Image.FLIP_LEFT_RIGHT)
-    out.save('transpose-output.jpg')
+    out.save(file_path)
     return out 
 
 ##### rotate #####
 
-def rotate(image_location, angle):
+def rotation(image_location, angle):
     expand = True
     im = Image.open(image_location)
     out = im.rotate(angle, expand=True)
-    out.save('rotate-output.png')
+    out.save(image_location)
     return out 
 
 ##### brightness #####
 
-def blend(image_location, brightness, value):  #### podeli input sa 100          
+def blend(image_location, value): 
+    #### podeli input sa 100
+    width, height = image_location.size
+    brightness = Image.new("RGB", (width, height), (255, 255, 255))          
     it1 = np.nditer(image_location)
     it2 = np.nditer(brightness)            
     for (x) in it1:
         for (y) in it2:
             newImage = (x + y) * value
-    return out
+    return newImage
 
 ##### contrast #####
 
-def contrast(image_location, value): #### podeli input sa 10
-    im = Image.open(image_location)
+def contrast(file_path, value): #### podeli input sa 10
+    im = Image.open(file_path)
     image_array = np.array(im)
     contrast_factor = value
     contrast_img = image_array * contrast_factor
     contrast_img = np.clip (contrast_img, 0, 255)
     out = Image.fromarray(contrast_img.astype(np.uint8))
-    out.save("output_image.jpg")
+    out.save(file_path)
     return out
 
 
--- gui.py
+++ gui.py
@@ -1,44 +1,126 @@
+from hashlib import new
+from multiprocessing.dummy import current_process
+from operator import ne
 from tkinter import *
 from tkinter.font import Font
 from tkinter import filedialog as fd
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageGrab
 from functions import *
 
-def openfile():
-    file_path = fd.askopenfilename()
-    print(file_path)
-    if file_path:
-        # Open and display the image using Pillow
-        img = Image.open(file_path)
-        img.thumbnail((600, 400))
-        img = ImageTk.PhotoImage(img)
-    
-        canvas.create_image(0, 0, anchor=NW, image=img)
-        canvas.img = img
+file_path = ""
+WIDTH = 600
+HEIGHT = 400
 
+
+#pravi inicijalnu belu sliku za canvas
+def get_image():
+    img = Image.new("RGB", (400, 400), "white")  
+    img_tk = ImageTk.PhotoImage(img)
+    return img_tk
+
+#funkcija za otvaranje fajla
+def openfile():
+    global file_path, current_image
+    file_path = fd.askopenfilename()
+    if file_path:
+        global image
+        image = Image.open(file_path)
+        ar = float(image.width / image.height)
+        new_width = int(ar*HEIGHT)
+        image = image.resize((new_width, HEIGHT), Image.LANCZOS)
+        #img.thumbnail((600, 400))
+        image = ImageTk.PhotoImage(image)
+        current_image = image
+    
+        canvas.create_image(0, 0, anchor=NW, image=current_image)
+
+#funkcija za cuvanje slike
+def saveimage():
+    global file_path
+    if file_path:
+        image = ImageGrab.grab(bbox=(canvas.winfo_rootx(), canvas.winfo_rooty(), canvas.winfo_rootx() + canvas.winfo_width(), canvas.winfo_rooty() + canvas.winfo_height()))
+        file_path = fd.asksaveasfilename(defaultextension=".jpg")
+        image.save(file_path)
+
+#funkcija koja otvara ili zatvara polje za unos
 def brightFunc():
     if frame_for_input.winfo_ismapped():
         frame_for_input.pack_forget()
     else:
         frame_for_input.pack()
     
-
+#funkcija koja uzima vrednost sa inputa i poziva funkciju za brajtnes
 def submit_brightness():
+    global file_path, current_image
     value = brightness_input.get()
-    print(value)
+    value = int(value)
+    value /= 100
+    current_image = blend(file_path, value)
+    ar = float(current_image.width / current_image.height)
+    new_width = int(ar * HEIGHT)
+    current_image = current_image.resize((new_width, HEIGHT), Image.LANCZOS)
+    current_image = ImageTk.PhotoImage(current_image)
+    canvas.delete("all")
+    canvas.create_image(0, 0, anchor=NW, image=current_image)
     frame_for_input.pack_forget()
 
-
+#funkcija koja otvara ili zatvara polje za unos
 def contrastFunc():
     if frame_for_input2.winfo_ismapped():
         frame_for_input2.pack_forget()
     else:
         frame_for_input2.pack()
 
+#funkcija koja uzima vrednost sa inputa, poziva funkciju contrast i ucitava novu sliku na canvas
 def submit_contrast():
-    value = contrast_input.get()
-    print(value)
+    global file_path, current_image
+    val = contrast_input.get()
+    val = int(val)
+    current_image = contrast(file_path, val/10)
+    ar = float(current_image.width / current_image.height)
+    new_width = int(ar * HEIGHT)
+    current_image = current_image.resize((new_width, HEIGHT), Image.LANCZOS)
+    current_image = ImageTk.PhotoImage(current_image)
+    canvas.delete("all")
+    canvas.create_image(0, 0, anchor=NW, image=current_image)
+
     frame_for_input2.pack_forget()
+
+
+#funkcija koja poziva funkciju za vertical flip i ucitava novu sliku
+def pozovivert():
+    global file_path, current_image
+    current_image = flip_vertically(file_path)
+    ar = float(current_image.width / current_image.height)
+    new_width = int(ar * HEIGHT)
+    current_image = current_image.resize((new_width, HEIGHT), Image.LANCZOS)
+    current_image = ImageTk.PhotoImage(current_image)
+    canvas.delete("all")
+    canvas.create_image(0, 0, anchor=NW, image=current_image)
+
+
+#funkcija koja poziva funkciju za horizontal flip i ucitava novu sliku
+def pozovihor():
+    global file_path, current_image
+    current_image = flip_horizontally(file_path)
+    ar = float(current_image.width / current_image.height)
+    new_width = int(ar * HEIGHT)
+    current_image = current_image.resize((new_width, HEIGHT), Image.LANCZOS)
+    current_image = ImageTk.PhotoImage(current_image)
+    canvas.delete("all")
+    canvas.create_image(0, 0, anchor=NW, image=current_image)
+
+def pozovirot(): 
+    global file_path, current_image
+    current_image = rotation(file_path, 90)
+    ar = float(current_image.width / current_image.height)
+    new_width = int(ar * HEIGHT)
+    current_image = current_image.resize((new_width, HEIGHT), Image.LANCZOS)
+    current_image = ImageTk.PhotoImage(current_image)
+    canvas.delete("all")
+    canvas.create_image(0, 0, anchor=NW, image=current_image)
+
+    
 
 window = Tk()
 window.title("Photoshop")
@@ -84,14 +166,16 @@ ic8 = PhotoImage(file="icons/rotate.png")
 rotate = Button(
     frame1,
     image=ic8,
-    borderwidth=0
+    borderwidth=0,
+    command=pozovirot
 ).pack(side=LEFT)
 
 ic3 = PhotoImage(file="icons/flip_horizontal.png")
 flipHorizontalBtn = Button(
     frame1,
     image=ic3,
-    borderwidth=0
+    borderwidth=0,
+    command=pozovihor
 )
 flipHorizontalBtn.pack(side=LEFT)
 
@@ -99,7 +183,8 @@ ic4 = PhotoImage(file="icons/flip_vertical.png")
 flipVerticalBtn = Button(
     frame1,
     image=ic4,
-    borderwidth=0
+    borderwidth=0,
+    command=pozovivert
 )
 flipVerticalBtn.pack(side=LEFT)
 
@@ -129,8 +214,8 @@ contrast_input = Entry(frame_for_input2)
 contrast_input.pack()
 
 ic6 = PhotoImage(file="icons/contrast.png")
-contrast = Button(frame1, image=ic6, borderwidth=0, command=contrastFunc)
-contrast.pack(side=LEFT)
+contrast_btn = Button(frame1, image=ic6, borderwidth=0, command=contrastFunc)
+contrast_btn.pack(side=LEFT)
 
 
 #Save
@@ -138,7 +223,8 @@ ic7 = PhotoImage(file="icons/save.png")
 save = Button(
     frame1,
     image=ic7,
-    borderwidth=0
+    borderwidth=0,
+    command=saveimage
 )
 save.pack(side=LEFT)
 
@@ -153,6 +239,9 @@ photoFrame.pack(fill=BOTH, expand=TRUE, padx=30, pady=30)  # Moved packing of ph
 
 canvas = Canvas(photoFrame, width=600, height=400) 
 canvas.pack(side=TOP)
+
+current_image = get_image()
+canvas.create_image(0, 0, anchor=NW, image=current_image)
 
 
 
